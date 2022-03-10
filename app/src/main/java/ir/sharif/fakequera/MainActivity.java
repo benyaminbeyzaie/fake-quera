1468257a68bc0fdccb1000ee0b4abad2b74258e4
Mojtaba <rastegar123456789@gmail.com>
2022-03-10 15:24:28 +0330

    tabBat added

--- app/src/main/java/ir/sharif/fakequera/MainActivity.java
+++ app/src/main/java/ir/sharif/fakequera/MainActivity.java
@@ -1,44 +1,34 @@
 package ir.sharif.fakequera;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.room.Room;
 
 import android.os.Bundle;
-import android.text.Editable;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.material.tabs.TabLayout;
 
-import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-import ir.sharif.fakequera.dao.UserDao;
-import ir.sharif.fakequera.database.AppDatabase;
 import ir.sharif.fakequera.entities.User;
 import ir.sharif.fakequera.viewModels.UserViewModel;
 
 public class MainActivity extends AppCompatActivity {
     UserViewModel userViewModel;
-    private RelativeLayout layout;
-    TextInputLayout usernameInput;
-    TextInputLayout passwordInput;
-
+    ConstraintLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         layout = findViewById(R.id.main_layout);
-        usernameInput = findViewById(R.id.username);
-        passwordInput = findViewById(R.id.password);
+
         userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
         // TODO: delete this line and add sign up page
         userViewModel.insert(new User("ben", "123"));
@@ -53,12 +43,31 @@ public class MainActivity extends AppCompatActivity {
                 showTopSnackBar("User authenticated failed");
             }
         });
-        findViewById(R.id.LoginButton).setOnClickListener(v -> {
-            Log.d("user", "try with:" + Objects.requireNonNull(usernameInput.getEditText()).getText().toString());
-            userViewModel.authenticate(Objects.requireNonNull(usernameInput.getEditText()).getText().toString(),
-                    Objects.requireNonNull(passwordInput.getEditText()).getText().toString());
-        });
 
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                System.out.println(tab.getId());
+                System.out.println(tab.getText());
+                if (Objects.requireNonNull(tab.getText()).equals(getString(R.string.login))){
+                    Toast.makeText(MainActivity.this, "login", Toast.LENGTH_SHORT).show();
+                }else if (tab.getText().equals(getString(R.string.signup))){
+                    Toast.makeText(MainActivity.this, "signup", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 
     void showTopSnackBar(String message) {
