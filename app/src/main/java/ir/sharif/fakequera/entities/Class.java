8329785f62195305e6deca31604f1cb7aa4655f6
Benyamin Beyzaie <benyamin.beyzaie@hezardastan.net>
2022-03-16 10:18:21 +0330

    add students field to class

--- app/src/main/java/ir/sharif/fakequera/entities/Class.java
+++ app/src/main/java/ir/sharif/fakequera/entities/Class.java
@@ -4,6 +4,8 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.util.ArrayList;
+
 @Entity
 public class Class {
     @PrimaryKey(autoGenerate = true)
@@ -11,4 +13,7 @@ public class Class {
 
     @ColumnInfo(name = "owner_teacher_id")
     public int ownerTeacherId;
+
+    @ColumnInfo(name = "students")
+    public ArrayList<Integer> students;
 }
