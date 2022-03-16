de1023c5f15733c917013eb7eb3f7acf7dfc0502
Benyamin Beyzaie <benyamin.beyzaie@hezardastan.net>
2022-03-16 10:17:59 +0330

    add class to data base

--- app/src/main/java/ir/sharif/fakequera/database/AppDatabase.java
+++ app/src/main/java/ir/sharif/fakequera/database/AppDatabase.java
@@ -5,22 +5,27 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import ir.sharif.fakequera.dao.ClassDao;
 import ir.sharif.fakequera.dao.StudentDao;
 import ir.sharif.fakequera.dao.TeacherDao;
 import ir.sharif.fakequera.dao.UserDao;
+import ir.sharif.fakequera.entities.Class;
 import ir.sharif.fakequera.entities.Student;
 import ir.sharif.fakequera.entities.Teacher;
 import ir.sharif.fakequera.entities.User;
 
-@Database(entities = {User.class, Student.class, Teacher.class}, version = 1)
+@Database(entities = {User.class, Student.class, Teacher.class, Class.class}, version = 1)
+@TypeConverters({Converters.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract UserDao userDao();
     public abstract StudentDao studentDao();
     public abstract TeacherDao teacherDao();
+    public abstract ClassDao boxDao();
 
     private static volatile AppDatabase INSTANCE;
     private static final int NUMBER_OF_THREADS = 4;
